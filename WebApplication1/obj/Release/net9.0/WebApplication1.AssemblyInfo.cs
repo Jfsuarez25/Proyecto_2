7b223c6130a5380bc651b5af31ca15c04d5570d9
Jfsuarez25 <juanpredator1@gmail.com>
2025-04-08 20:37:06 -0500

    Se realiza el cambio. Se agrega más texto

--- WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Release/net9.0/WebApplication1.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+379516aa82be610fc4b30bddfd2fe17302b172c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
